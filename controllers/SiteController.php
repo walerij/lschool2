c58ab54ca2c825c8f487e0b7dfa7fd70d7fcd1db
Administrator <becwal@yandex.ru>
2017-08-14 00:14:30 +0300

    add sitecontroller исправление ошибки

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,19 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 14.08.2017
- * Time: 0:05
- */
+
 
 namespace  app\controllers;
 use yii\web\Controller;
 
 class SiteController extends Controller
 {
-    public function actionsIndex()
+    public function actionIndex()
     {
-        return "Video School ver 1.0.0";
+        return 'Video School ver 1.0.0';
     }
 
 }
