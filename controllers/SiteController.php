b64632b392217a9b70126ace23966da29c8b87e9
Administrator <becwal@yandex.ru>
2017-08-14 00:10:37 +0300

    add sitecontroller

---
+++ controllers/SiteController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14.08.2017
+ * Time: 0:05
+ */
+
+namespace  app\controllers;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionsIndex()
+    {
+        return "Video School ver 1.0.0";
+    }
+
+}
