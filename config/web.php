18dde4051c21157adbcddf8959a4e33effa6dc75
Administrator <becwal@yandex.ru>
2017-08-16 20:05:13 +0300

    add debug line

--- config/web.php
+++ config/web.php
@@ -2,11 +2,20 @@
   return [
       'id' =>'l-school',
       'basePath'=>realpath(__DIR__.'/../'),
+      'bootstrap' => ['debug'],
       'components'=>[
           'urlManager' =>[
               'enablePrettyUrl'=>true,
               'showScriptName' =>false
-          ]
+          ],
+          'request'=>['cookieValidationKey'=>'fkmj0k']
+
       ]
-   ];
+      ,
+      'modules' =>['debug'=>[
+          'class' => 'yii\debug\Module',]
+      ]
+
+
+  ];
 ?>
