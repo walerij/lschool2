6ed6779aaaeabc9482bceb739c199514b75c7024
Administrator <becwal@yandex.ru>
2017-08-30 23:46:29 +0300

    add user step object

--- Views/User/join.php
+++ Views/User/join.php
@@ -1,1 +1,15 @@
-<h1>JOIN US</h1>
+<div class="panel panel-danger">
+    <div class="panel panel-heading">
+        <h1>JOIN US</h1>
+    </div>
+    <div class="panel-body">
+        name<br>
+        email<br>
+        password<br>
+        password2<br>
+        [Create]
+
+    </div>
+
+</div>
+
--- Views/User/login.php
+++ Views/User/login.php
@@ -1,1 +1,12 @@
-<h1>LOGIN US</h1>
+<div class="panel panel-info">
+    <div class="panel panel-heading">
+        <h1>LOG IN</h1>
+    </div>
+    <div class="panel-body">
+        email<br>
+        password<br>
+        [ENTER]
+
+    </div>
+
+</div>
