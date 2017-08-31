fcbe680c53d40e7259082047ca4e645dddcb97e6
Administrator <becwal@yandex.ru>
2017-08-31 09:32:31 +0300

    add database user connerction

--- config/console.php
+++ config/console.php
@@ -1,5 +1,8 @@
 <?php
 return[
     "id"=>"school-console",
- "basePath"=>dirname(__DIR__),
+    "basePath"=>dirname(__DIR__),
+    'components'=>[
+        'db'=>require (__DIR__.'/db.php')
+    ]
 ];
