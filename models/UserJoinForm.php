e6c5376d978c5370956a9cf9347748098c3266df
Administrator <becwal@yandex.ru>
2017-10-12 23:04:28 +0300

    валидация

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -10,4 +10,18 @@ class UserJoinForm extends Model
     public $password;
     public $password2;
 
+    public function rules()
+    {
+        return [
+            ['name','required'],
+            ['email','required'],
+            ['password','required'],
+            ['password2','required'],
+            ['name','string','min'=>3,'max'=>30, 'message'=>'ого, скока символов'],
+            ['email','email','message'=>'Не емейл'],
+            ['password','string','min'=>4, 'message'=>'Маловато будет символов в пароле'],
+            ['password2','compare','compareAttribute'=>'password','message'=>'пароли не равны'],
+        ];
+    }
+
 }
